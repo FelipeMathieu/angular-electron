14d1a4e9c4056cfa3de52e3f463cec8764e0e6e6
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-07-17 13:17:42 -0300

    Scaffolding the project
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

---
+++ electron/main.ts
@@ -0,0 +1,40 @@
+import electron from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+import reloader from 'electron-reloader';
+
+const { app, BrowserWindow } = electron;
+
+try {
+  reloader(module);
+} catch (_) {
+  console.log('Failed to load reloader');
+}
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const indexPath =
+    process.env['ELECTRON_START_URL'] ||
+    url.format({
+      pathname: path.join(__dirname, '../dist/browser/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    });
+
+  mainWindow.loadURL(indexPath);
+  mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
