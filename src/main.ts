4c5d003b945b49fdd5f50c1141ac07429776b64e
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-07-17 14:15:28 -0300

    Add right version of fontawesome
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { setupFontAwesomeIcons } from './app/fontawesome.config';
 
-bootstrapApplication(AppComponent, { ...appConfig }).catch((err) =>
+setupFontAwesomeIcons();
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
